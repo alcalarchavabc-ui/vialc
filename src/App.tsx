f262585e43bb72c8427f6c8d192425e88c89f364
Salvador Manuel Alcala Ruano <alcalarchavabc@gmail.com>
2026-06-02 18:36:59 -0600

    descripción del cambio

--- src/App.tsx
+++ src/App.tsx
@@ -5,5 +5,10 @@ import VialcBrief from './VialcBrief'
 export default function App() {
   const params = new URLSearchParams(window.location.search)
   if (params.get('page') === 'brief') return <VialcBrief />
-  return <VialcHero />
+  return (
+    <>
+      <VialcHero />
+      <VialcAbout />
+    </>
+  )
 }
